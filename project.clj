7ba7276171e81768a4923ede5f05c3c3c14c3457
Michał Guminiak <michal.guminiak@tomtom.com>
2013-10-14 19:08:21 +0200

    leiningen 2.0 should be used to build this app

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure "1.1.4"]
                  [ring/ring-jetty-adapter "1.1.6"]
