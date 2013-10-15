7a4a7d0bf644abc2e61ea1216cd32826631d66c0
Michał Guminiak <michal.guminiak@tomtom.com>
2013-10-15 22:07:29 +0200

    Project.clj cleanup

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject finagent "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject fin-agent "0.1.0-SNAPSHOT"
+  :description "Utility service for handling finance ops."
+  :url "http://fanhaf-fin-agent.herokuapp.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
