cee640089dcd08524aa1790928956b610f58b7ab
Michał Guminiak <michal.guminiak@tomtom.com>
2014-02-02 21:43:58 +0100

    Adding tests to mbank-parser

--- spec/fin_agent/parsers/mbank_spec.clj
+++ spec/fin_agent/parsers/mbank_spec.clj
@@ -1,10 +1,62 @@
 (ns fin-agent.parsers.mbank-spec
-  (:require [speclj.core :refer :all]))
+  (:use [fin-agent.parsers.mbank])
+  (:require [speclj.core :refer :all]
+            [clojure-csv.core :as csv]))
+
+(def mbank-report-csv
+  "l1
+l2;l3
+h1;h2;h3;h4;h5;h6;h7;h8;h9
+2014-01-25;2014-01-25;ZAKUP  PRZY UZYCIU KARTY;\"INDIE     GO GO\";\"  \";'';-2 060,50;56 403,14;
+2014-01-27;2014-01-27;ZAKUP  PRZY UZYCIU KARTY;\"CARREFOUR EXPRESS\";\"  \";'';-22,71;56 380,43;
+f1;f2;f3;f4;f5;f6;f7;f8;f9
+l5
+l6")
+
+(def mbank-report (csv/parse-csv mbank-report-csv :delimiter \;))
+(def operations-source (filter-operations-source mbank-report))
 
 
 (describe "CSV filter functions"
-  (it "should locate operation section in parsed csv report"
-    (should (= 1 0))))
+          (it "should locate operation sections with 9 columns in parsed csv report"
+              (should 
+                (= (count (first operations-source)) 9)
+                )))
+
+(describe "CSV filter functions"
+          (it "should locate two records of operations in operation test data"
+              (should
+                (= (count operations-source) 2))))
+
+(describe "CSV filter functions"
+          (it "the first string of the first record in test data should be \"2014-01-25\""
+              (should (= (first (first operations-source))
+                         "2014-01-25"))))
+
+(def operation-record (first operations-source))
+
+(describe "Operation type parsing"
+          (it "should find that operation-type in test record is \"ZAKUP PRZY UZYCIU KARTY\""
+              (should (= (operation-type operation-record)
+                         "ZAKUP PRZY UZYCIU KARTY"))))
+
+(describe "Operation description parsing"
+          (it "should find that operation-description in test record is \"INDIE GO GO\""
+              (should (= (operation-description operation-record)
+                         "INDIE GO GO"))))
+
+(describe "Operation amount parsing"
+          (it "should filter whitespaces in amount string"
+              (should (= (prepare-amount "-2 060,50")
+                         "-2060,50"))))
+
+(def parsed-operations
+  {:date "2014-01-25" :amount "-2060,50" :description "ZAKUP PRZY UZYCIU KARTY: INDIE GO GO"})
+
+(describe "to-operation function"
+          (it "should prepare a correct map of values from test operation source"
+              (should (= (to-operation operation-record)
+                         parsed-operations))))
 
 (run-specs)
 
--- src/fin_agent/parsers/mbank.clj
+++ src/fin_agent/parsers/mbank.clj
@@ -1,6 +1,7 @@
 (ns fin-agent.parsers.mbank
   (:use [clojure.string :only (join split)]))
 
+;####################
 (defn filter-operation-columns [report] (filter #(= (count %) 9) report))
 (defn truncate-header [operation-report] (drop 1 operation-report))
 (defn truncate-footer [operation-report] (drop-last operation-report))
@@ -10,6 +11,7 @@
     filter-operation-columns
     truncate-header
     truncate-footer))
+;####################
 
 (defn filter-whitespaces [string] (filter #(not= \space %) string))
 
@@ -20,12 +22,14 @@
 
 (defn operation-type [trans-src] (filter-duplicate-whitespace (nth trans-src 2)))
 (defn operation-description [trans-src] (filter-duplicate-whitespace (nth trans-src 3)))
+;####################
 
 (defn prepare-amount [amount-string] (apply str (filter-whitespaces amount-string)))
 (defn prepare-description [trans-src]
   (let [op-type (operation-type trans-src)
         op-desc (operation-description trans-src)]
     (apply str op-type ": " op-desc)))
+;####################
 
 (defn to-operation [trans-src]
   (let [date (nth trans-src 1)
