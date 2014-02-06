25cb8d82be9ec8b3d8ea818f492399b31ff86178
Michał Guminiak <michal.guminiak@tomtom.com>
2014-02-06 22:46:31 +0100

    Using mbank and pekao routes in core

--- src/fin_agent/core.clj
+++ src/fin_agent/core.clj
@@ -1,13 +1,16 @@
 (ns fin-agent.core
   (:use [compojure.core :only (defroutes)])
-  (:require [fin-agent.controllers.csvtranslator :as translator]
+  (:require [fin-agent.controllers.mbank :as mbank-controller]
+            [fin-agent.controllers.pekao :as pekao-controller]
             [ring.middleware.params :as params]
             [ring.middleware.multipart-params :as multipart-params]
             [ring.adapter.jetty :as jetty])
   )
 
 (defroutes routes
-  translator/routes)
+  mbank-controller/routes
+  pekao-controller/routes
+  )
 
 (def application (-> routes
                    params/wrap-params
