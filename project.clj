bf1dca2576f2ad9db62e9820febc8b113aac15ae
Michal Guminiak <Michal.Guminiak@tomtom.com>
2020-12-13 12:49:08 +0100

    Refreshing to java 8 and new version of speclj

--- project.clj
+++ project.clj
@@ -4,16 +4,16 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.1.4"]
                  [ring/ring-jetty-adapter "1.1.6"]
                  [hiccup "1.0.3"]
                  [clojure-csv/clojure-csv "2.0.1"]
                  ]
-  :dev-dependencies [[speclj "2.9.0"]]
-  :test-paths ["spec/"]
+  :profiles {:dev {:dependencies [[speclj "3.3.0"]]}}
+  :test-paths ["spec"]
   :plugins [[lein-ring "0.7.5"]
-            [speclj "2.9.0"]]
+            [speclj "3.3.0"]]
   :ring {:handler fin-agent.core/application}
   :uberjar-name "fin-agent-standalone.jar"
 )
